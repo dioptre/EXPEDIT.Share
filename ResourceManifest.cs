fe5b7ddb08a014f3fc8a62a9b0014c61b0238ed4
Andrew Grosser <dioptre@gmail.com>
2014-04-16 19:59:45 +1000

    wiki working

--- ResourceManifest.cs
+++ ResourceManifest.cs
@@ -4,6 +4,7 @@ namespace EXPEDIT.Share {
     public class ResourceManifest : IResourceManifestProvider {
         public void BuildManifests(ResourceManifestBuilder builder) {
             builder.Add().DefineStyle("PickFile").SetUrl("pickfile.css");
+            builder.Add().DefineStyle("Share").SetUrl("expedit-share.css");
         }
     }
 }
