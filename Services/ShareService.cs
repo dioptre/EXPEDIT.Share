6a25714fd31e8cb7cb422628848293eb8451af6c
Andrew Grosser <dioptre@gmail.com>
2014-05-20 17:18:16 +1000

    simplified upload file

--- Services/ShareService.cs
+++ Services/ShareService.cs
@@ -660,15 +660,8 @@ namespace EXPEDIT.Share.Services {
 
         public bool SubmitPhoto(HttpPostedFileBase file)
         {
-            if (file.ContentType.ToLower() != "image/jpg" &&
-            file.ContentType.ToLower() != "image/jpeg" &&
-            file.ContentType.ToLower() != "image/pjpeg" &&
-            file.ContentType.ToLower() != "image/gif" &&
-            file.ContentType.ToLower() != "image/x-png" &&
-            file.ContentType.ToLower() != "image/png")
-            {
+            if (!file.ContentType.ToLower().StartsWith("image"))
                 return false;
-            }
             var img = file.InputStream.ToByteArray();
             if (img == null || img.Length < 1)
                 return false;
