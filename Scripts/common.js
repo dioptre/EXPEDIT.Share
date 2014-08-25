3f66523011f1bb345e0769e488e8a82613d146c7
Andrew Grosser <dioptre@gmail.com>
2014-08-25 14:28:51 +1000

    updated js shared lib

--- Scripts/common.js
+++ Scripts/common.js
@@ -130,3 +130,14 @@ function TraverseDom(node, func) {
         node = node.nextSibling;
     }
 }
+
+
+function queryParamsLookup(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+    var results = regex.exec(window.location.href);
+    if (results == null)
+        return null;
+    else
+        return results[1];
+}
